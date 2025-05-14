203c35d38dc0fd3657e451acb54b800a93529819
Copter <sajawat_copter@hotmail.com>
2025-05-15 01:00:14 +0700

    feat: Add notice

--- src/views/SharedView.vue
+++ src/views/SharedView.vue
@@ -206,6 +206,14 @@ const closePaymentModal = () => {
   <div>
     <div class="a-card mb-6">
       <h1 class="text-xl font-bold text-neutral-700">ข้อมูลการชำระเงิน</h1>
+      <p class="text-yellow-600 text-sm mt-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-4 h-4 inline-block mr-1">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+        </svg>
+        <span>โปรดทราบ: ข้อมูลการชำระเงินที่แชร์ไปจะไม่อัพเดทอัตโนมัติ</span>
+      </p>
     </div>
 
     <!-- Loading State -->
@@ -368,7 +376,7 @@ const closePaymentModal = () => {
         </button>
       </div>
       <p class="text-center text-neutral-600 mb-1">สำหรับ: <span class="font-medium">{{ showQrCodeModalForPayer.name
-          }}</span></p>
+      }}</span></p>
       <p v-if="typeof showQrCodeModalForPayer.unpaidAmountDue === 'number' && showQrCodeModalForPayer.unpaidAmountDue < showQrCodeModalForPayer.totalAmountDue"
         class="text-center text-xs text-accent mb-2">
         แสดงเฉพาะยอดที่ยังไม่ได้จ่าย
